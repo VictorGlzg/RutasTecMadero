1fffe66fe593c09487b494578cdbb4ac8cd52f58
Victor González <victorglzgmz12@gmail.com>
2023-11-28 23:52:31 -0600

    nav bottom + icons

--- RutasTecMadero/app/src/main/java/com/example/rutastecmadero/MapFragment.kt
+++ RutasTecMadero/app/src/main/java/com/example/rutastecmadero/MapFragment.kt
@@ -1,25 +1,35 @@
 package com.example.rutastecmadero
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolygonOptions
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.gms.maps.model.RoundCap
 
 class MapFragment : Fragment(), GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
     private lateinit var mMap: GoogleMap
     private lateinit var supportMapFragment : SupportMapFragment
+    companion object{
+        const val REQUEST_CODE_LOCATION = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -27,23 +37,26 @@ class MapFragment : Fragment(), GoogleMap.OnMapClickListener, GoogleMap.OnMapLon
         val view = inflater.inflate(R.layout.fragment_map, container, false)
 
         supportMapFragment = childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
-
+        //onMapReady
         supportMapFragment.getMapAsync{
             mMap = it
             this.mMap.setOnMapClickListener(this)
             this.mMap.setOnMapLongClickListener(this)
-
+            //mMap.setMinZoomPreference(17.8f)
+            drawPolygon()
             createPolylines()
 
             val campus2 = LatLng(22.257027863336113, -97.85040616776612)
             mMap.moveCamera(CameraUpdateFactory.newLatLng(campus2))
 
-            val uas = LatLng(22.256417723605228, -97.85059487755778)
+            //val uas = LatLng(22.256417723605228, -97.85059487755778)
 
-            val options = MarkerOptions().position(uas).title("Unidad Academica de Sistemas")
-            options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE))
+            //val options = MarkerOptions().position(uas).title("Unidad Academica de Sistemas")
+            //options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE))
 
-            mMap.addMarker(options)
+            //mMap.addMarker(options)
+
+            enableLocation()
         }
         return view
     }
@@ -58,10 +71,45 @@ class MapFragment : Fragment(), GoogleMap.OnMapClickListener, GoogleMap.OnMapLon
             .add(LatLng(22.256400808003363, -97.85021635687629))
             .add(LatLng(22.256536942522885, -97.85049413390621))
             .add(LatLng(22.256430262167527, -97.85055661299623))
-            //camino hacia biblioteca y centro de computo
+            //camino hacia biblioteca
+            .add(LatLng(22.256430262167527, -97.85055661299623))
+            .add(LatLng(22.256536942522885, -97.85049413390621))
+            .add(LatLng(22.256537933498763, -97.85049364542438))
+            .add(LatLng(22.256681637209624, -97.85076698466938))
+            .add(LatLng(22.256682266310577, -97.85076704466238))
+            .add(LatLng(22.256899799040013, -97.85063344925713))
+            .add(LatLng(22.256992074679673, -97.85081845136175))
+            .add(LatLng(22.257038092023308, -97.85079126084298))
+            //camino hacia sistemas
+            .add(LatLng(22.257038092023308, -97.85079126084298))
+            .add(LatLng(22.256992074679673, -97.85081845136175))
+            .add(LatLng(22.25699247191386, -97.8508176295884))
+            .add(LatLng(22.257025248948878, -97.85087819553304))
+            .add(LatLng(22.256916497083395, -97.8509536209512))
+            //camino hacia industrial
+            .add(LatLng(22.256916497083395, -97.8509536209512))
+            .add(LatLng(22.257025248948878, -97.85087819553304))
+            .add(LatLng(22.257025329492876, -97.8508778357832))
+            .add(LatLng(22.257214451907643, -97.85123880411443))
+            .add(LatLng(22.257090342184696, -97.85131860480125))
+            //regreso
+            .add(LatLng(22.257090342184696, -97.85131860480125))
+            .add(LatLng(22.257214451907643, -97.85123880411443))
+            .add(LatLng(22.256899621338377,-97.85063335691588))
+            .add(LatLng(22.256877930231028,-97.85059053019027))
+            //hacia maestria
+            .add(LatLng(22.257427956010844,-97.85024899893692))
+            //labs 3 y 4 petroquimica
+            .add(LatLng(22.257590959655104,-97.85055786828768))
+            //labs 1 y 2 petroquimica
+            .add(LatLng(22.257299728852885,-97.85001043702715))
+            //ed QQ
+            .add(LatLng(22.257434021224043,-97.8499072491608))
+            //ed HH
+            .add(LatLng(22.257765126323974,-97.85053103007746))
 
             .width(12f)
-            //.color(ContextCompat.getColor(this,R.color.cyan))
+            .color(ContextCompat.getColor(requireActivity().baseContext,R.color.cyan))
 
         val polyline = mMap.addPolyline(polylineOptions)
         polyline.startCap = RoundCap()
@@ -75,5 +123,55 @@ class MapFragment : Fragment(), GoogleMap.OnMapClickListener, GoogleMap.OnMapLon
     override fun onMapLongClick(p0: LatLng) {
     }
 
+    private fun isLocationPermissionGranted()=
+        ContextCompat.checkSelfPermission(requireActivity().baseContext, Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED
+        @SuppressLint("MissingPermission")
+        private fun enableLocation(){
+            if(!::mMap.isInitialized) return
+            if(isLocationPermissionGranted()){
+                mMap.isMyLocationEnabled = true
+            }
+            else{
+                requestLocationPermission()
+            }
+        }
 
+    private fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(requireActivity().baseContext,"Falta de autorizar los permisos de localizacion", Toast.LENGTH_SHORT).show()
+        }else{
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_CODE_LOCATION)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                mMap.isMyLocationEnabled = true
+            }
+            else{
+                Toast.makeText(requireActivity().baseContext,"Activar la localizacion desde ajustes", Toast.LENGTH_SHORT).show()
+            }
+            else -> {}
+        }
+    }
+
+    private fun drawPolygon(){
+        mMap.clear()
+        val polygon = PolygonOptions()
+        .add(LatLng(22.256822042377024, -97.85043489343123))
+        .add(LatLng(22.25676885872791, -97.850435947836))
+        .add(LatLng(22.256722262028234, -97.85035106826841))
+        .add(LatLng(22.256738363507154, -97.85032418095201))
+        .add(LatLng(22.25677056646009, -97.85033235258744))
+        .add(LatLng(22.256822042377024, -97.85043489343123))
+
+        polygon.fillColor(R.color.cyan)
+        mMap.addPolygon(polygon)
+    }
 }
--- RutasTecMadero/app/src/main/java/com/example/rutastecmadero/PrincipalActivity.kt
+++ RutasTecMadero/app/src/main/java/com/example/rutastecmadero/PrincipalActivity.kt
@@ -5,32 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.example.rutastecmadero.databinding.ActivityPrincipalBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class PrincipalActivity : AppCompatActivity(){
-
-    lateinit var btnChatbot : Button
-    lateinit var btnMap : Button
-    lateinit var btnConfig : Button
-
+    lateinit var binding : ActivityPrincipalBinding
+    //val bNavegationView = findViewById<BottomNavigationView>(R.id.navBar)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_principal)
-        //replaceFragment(MapFragment())
+        binding = ActivityPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnChatbot = findViewById(R.id.chatbot)
-        btnMap = findViewById(R.id.mapa)
-        btnConfig = findViewById(R.id.conf)
+        binding.navBar.menu.findItem(R.id.mapaOpc).isChecked = true
 
-        btnChatbot.setOnClickListener {
-            replaceFragment(ChatbotFragment())
-        }
-
-        btnMap.setOnClickListener {
-            replaceFragment(MapFragment())
-        }
-
-        btnConfig.setOnClickListener {
-            replaceFragment(ConfigFragment())
+        binding.navBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.chatbotOpc -> replaceFragment(ChatbotFragment())
+                R.id.mapaOpc -> replaceFragment(MapFragment())
+                R.id.configOpc -> replaceFragment(ConfigFragment())
+                else -> {}
+            }
+            true
         }
 
     }
@@ -40,42 +35,5 @@ class PrincipalActivity : AppCompatActivity(){
         supportFragmentManager.beginTransaction().replace(R.id.fragmentPrincipal,f).commit()
     }
 
-
-
-
-
-//    override fun onMapClick(p0: LatLng) {
-//
-//    }
-//
-//    override fun onMapLongClick(p0: LatLng) {
-//
-//    }
-
-
-//    private fun createPolylines(){
-//        //Trazar las rutas del campus 2
-//        val polylineOptions = PolylineOptions()
-//            //entrada rumbo a UAS
-//            .add(LatLng(22.256618058457427, -97.84997241437556))
-//            .add(LatLng(22.256640399837003, -97.85007433832214))
-//            .add(LatLng(22.256407056363226, -97.85022722424202))
-//            .add(LatLng(22.256544207757777, -97.85048203410194))
-//            .add(LatLng(22.25644305085775, -97.85055378319592))
-//            //camino hacia biblioteca y centro de computo
-//            .add(LatLng(22.256544207757777, -97.85048203410194))
-//            .add(LatLng(22.256703079798328, -97.85075561942472))
-//            .add(LatLng(22.256910978489174, -97.85063626111089))
-//            .add(LatLng(22.25701709978844, -97.8508682721998))
-//            .add(LatLng(22.256906634340698, -97.85093465687554))
-//
-//            .width(12f)
-//            .color(ContextCompat.getColor(this,R.color.cyan))
-//
-//        val polyline = mMap.addPolyline(polylineOptions)
-//        polyline.startCap = RoundCap()
-//        polyline.endCap = RoundCap()
-//
-//    }
 
 }
